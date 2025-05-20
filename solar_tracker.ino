6628c6bc1577001c1cf1c41095122769cccc9574
Ivan Perchuklieski <ivanperchuklieski@gmail.com>
2025-05-20 22:18:38 +0200

    Solar tracker code

---
+++ solar_tracker.ino
@@ -0,0 +1,52 @@
+#include <Servo.h>
+
+const int light1Pin = A0;
+const int light2Pin = A1;
+const int solarPin = A2;
+const int servoPin = 9;
+
+int angle = 90;
+int light1Reading;
+int light2Reading;
+int difference;
+int margin = 20;
+int solarReading;
+float solarVoltage;
+
+Servo servo1;
+
+void setup() {
+  servo1.attach(servoPin);
+  servo1.write(angle);
+  Serial.begin(9600);
+}
+
+void loop() {
+  light1Reading = analogRead(light1Pin);
+  light2Reading = analogRead(light2Pin);
+  difference = light2Reading - light1Reading;
+
+  if (difference > margin) {
+    if (angle < 180) {
+      angle = angle + 1;
+    }
+  } else if (difference < -margin) {
+    if (angle > 0) {
+      angle = angle - 1;
+    }
+  }
+
+  servo1.write(angle);
+
+  solarReading = analogRead(solarPin);
+  solarVoltage = solarReading * 5.0 / 1023;
+
+  Serial.print("Sensor 1 reading: ");
+  Serial.print(light1Reading);
+  Serial.print(" Sensor 2 reading: ");
+  Serial.print(light2Reading);
+  Serial.print(" Solar panel voltage: ");
+  Serial.println(solarVoltage);
+
+  delay(100);
+}
